83f2892639c3739d5358ecbebef3ad90cdbc51fd
LGarza94 <161745045+LGarza94@users.noreply.github.com>
2024-09-25 00:35:40 -0500

    updated a few files
    added code to base.html in order to flash user in webpage that their data is incorrect, displays a flash message is data was entered correctly. Modified a line of code within base.html in order to properly display success message

--- website/auth.py
+++ website/auth.py
@@ -22,13 +22,12 @@ def sign_up():
         if len(email) < 4:
             flash('Email must be greater than 3 characters.', category='error')
         elif len(firstName) < 2:
-            flash('First name must be greater than 1 characters.', category='error')
+            flash('First name must be greater than 1 character.', category='error')
         elif password1 != password2:
             flash('Passwords don\'t match.', category='error')
         elif len(password1) < 7:
             flash('Password must be at least 7 characters.', category='error')
         else:
-            #add user to database
-            flash('Account created!.', category='success')
+            flash('Account created!', category='success')
 
     return render_template("sign_up.html")
